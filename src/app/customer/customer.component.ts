0f18c3dc669e8faf76fd6c5e06f8763cab3b5ee1
Kay <info@beckmann-md.de>
2024-05-24 17:28:53 +0200

    read DB and show data

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { liveQuery } from 'dexie';
-import {db, Customer} from '../services/db.service';
+import { db, Customer } from '../services/db.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './customer.component.html',
-  styleUrl: './customer.component.scss'
+  styleUrl: './customer.component.scss',
 })
 export class CustomerComponent {
- customers$= liveQuery(() => db.customers.toArray())
- name: string ='';
- description: string ='';
+  customers$ = liveQuery(() => db.customers.toArray());
+  name: string = '';
+  description: string = '';
 
- async addNewCustomer(){
-  await db.customers.add({
-    name: this.name,
-    description: this.description
-  })
- }
+  async addNewCustomer() {
+    await db.customers.add({
+      name: this.name,
+      description: this.description,
+    });
+
+    this.name = '';
+    this.description = '';
+  }
 }
